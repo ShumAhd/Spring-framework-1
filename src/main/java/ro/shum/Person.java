66713570ee400d2a884bdabb23db40b781ebce5c
ShuMAhd <shuman@zykaika.ru>
2024-01-02 06:52:03 +0800

    Написан код в class App

--- src/main/java/ro/shum/Person.java
+++ src/main/java/ro/shum/Person.java
@@ -3,9 +3,16 @@ package ro.shum;
 import java.io.Serializable;
 
 public class Person implements Serializable {
+
   private String name;
   private int age;
 
+
+  public void setName(String john) {
+  }
+
+  public void setAge(int i) {
+  }
 
   @Override
   public String toString() {
